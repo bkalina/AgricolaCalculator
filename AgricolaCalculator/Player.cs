af065c1098e941b0acbddc9ab92a3e341e018cb3
Bartosz Kalina <bartosz.kalina@javamylove.pl>
2015-03-03 15:59:40 +0100

    Dodanie pozostałych przycisków, dodanie wyliczenia wyniku

--- AgricolaCalculator/Player.cs
+++ AgricolaCalculator/Player.cs
@@ -16,29 +16,30 @@ namespace AgricolaCalculator
     {
         public string name { get; set; }
         public int score { get; set; }
-
-        public int fields { get; set; }
-        public int pastures { get; set; }
-        public int grain { get; set; }
-        public int vegetables { get; set; }
-        public int sheep { get; set; }
-        public int wildBoar { get; set; }
-        public int cattle { get; set; }
-        public int fencedStables { get; set; }
-        public int unusedSpaces { get; set; }
-        public int rooms { get; set; }
-        public int roomType { get; set; }
-        public int familyMembers { get; set; }
-        public int bonusPoints { get; set; }
-        public int beggingCards { get; set; }
-
         public List<int> pointsList { get; set; }
+
+        private int fields = -1;
+        private int pastures = -1;
+        private int grain = -1;
+        private int vegetables = -1;
+        private int sheep = -1;
+        private int wildBoar = -1;
+        private int cattle = -1;
+        private int fencedStables = 0;
+        private int roomType = 0;
+        private int familyMembers = 6;
+        private int beggingCards = 0;
+        private int unusedSpaces = 0;
+        private int rooms = 2;
+        private int cardsPoints = 0;
+        private int bonusPoints = 0;
 
         public Player(string name)
         {
             this.name = name;
             this.score = 0;
-            pointsList = new List<int> { fields, pastures, grain, vegetables, sheep, wildBoar, cattle, fencedStables, unusedSpaces, rooms, roomType, familyMembers, bonusPoints, beggingCards };
+
+            pointsList = new List<int> { fields, pastures, grain, vegetables, sheep, wildBoar, cattle, fencedStables, roomType, familyMembers, beggingCards, unusedSpaces, rooms, cardsPoints, bonusPoints };
         }
 
     }
--- AgricolaCalculator/Score.xaml.cs
+++ AgricolaCalculator/Score.xaml.cs
@@ -19,7 +19,7 @@ namespace AgricolaCalculator
         private Player player;
         private string selectedPlayer;
         private List<List<ScoreButton>> guiList;
-        private List<ScoreButton> fieldstBtnList, pasturesBtnList, grainBtnList, vegetablesBtnList, sheepBtnList, wildBoarBtnList, cattleBtnList, fencedStablesBtnList;
+        private List<ScoreButton> fieldstBtnList, pasturesBtnList, grainBtnList, vegetablesBtnList, sheepBtnList, wildBoarBtnList, cattleBtnList, fencedStablesBtnList, roomTypeBtnList, familyMembersBtnList, beggingCardsBtnList;
 
         public Score()
         {
@@ -32,9 +32,12 @@ namespace AgricolaCalculator
             sheepBtnList = new List<ScoreButton> { new ScoreButton(-1, sheepBtn1), new ScoreButton(1, sheepBtn2), new ScoreButton(2, sheepBtn3), new ScoreButton(3, sheepBtn4), new ScoreButton(4, sheepBtn5) };
             wildBoarBtnList = new List<ScoreButton> { new ScoreButton(-1, wildBoarBtn1), new ScoreButton(1, wildBoarBtn2), new ScoreButton(2, wildBoarBtn3), new ScoreButton(3, wildBoarBtn4), new ScoreButton(4, wildBoarBtn5) };
             cattleBtnList = new List<ScoreButton> { new ScoreButton(-1, cattleBtn1), new ScoreButton(1, cattleBtn2), new ScoreButton(2, cattleBtn3), new ScoreButton(3, cattleBtn4), new ScoreButton(4, cattleBtn5) };
-            fencedStablesBtnList = new List<ScoreButton> { new ScoreButton(-1, fencedStablesBtn1), new ScoreButton(1, fencedStablesBtn2), new ScoreButton(2, fencedStablesBtn3), new ScoreButton(3, fencedStablesBtn4), new ScoreButton(4, fencedStablesBtn5) };
+            fencedStablesBtnList = new List<ScoreButton> { new ScoreButton(0, fencedStablesBtn1), new ScoreButton(1, fencedStablesBtn2), new ScoreButton(2, fencedStablesBtn3), new ScoreButton(3, fencedStablesBtn4), new ScoreButton(4, fencedStablesBtn5) };
+            roomTypeBtnList = new List<ScoreButton> { new ScoreButton(0, roomTypeBtn1), new ScoreButton(1, roomTypeBtn2), new ScoreButton(2, roomTypeBtn3) };
+            familyMembersBtnList = new List<ScoreButton> { new ScoreButton(6, familyMembersBtn1), new ScoreButton(9, familyMembersBtn2), new ScoreButton(12, familyMembersBtn3), new ScoreButton(15, familyMembersBtn4) };
+            beggingCardsBtnList = new List<ScoreButton> { new ScoreButton(0, beggingCardsBtn1), new ScoreButton(-3, beggingCardsBtn2), new ScoreButton(-6, beggingCardsBtn3), new ScoreButton(-9, beggingCardsBtn4), new ScoreButton(-12, beggingCardsBtn5), new ScoreButton(-15, beggingCardsBtn6) };
 
-            guiList = new List<List<ScoreButton>> { fieldstBtnList, pasturesBtnList, grainBtnList, vegetablesBtnList, sheepBtnList, wildBoarBtnList, cattleBtnList, fencedStablesBtnList };
+            guiList = new List<List<ScoreButton>> { fieldstBtnList, pasturesBtnList, grainBtnList, vegetablesBtnList, sheepBtnList, wildBoarBtnList, cattleBtnList, fencedStablesBtnList, roomTypeBtnList, familyMembersBtnList, beggingCardsBtnList };
 
             Loaded += (sender, e) =>
             {
@@ -52,7 +55,27 @@ namespace AgricolaCalculator
 
         private void countTotalScore()
         {
-            player.score = player.pointsList.Sum();
+            int score = 0;
+            // punkty podstawowe
+            for (int i = 0; i <= 7; i++)
+            {
+                score += player.pointsList[i];
+            }
+            // rodzina
+            score += player.pointsList[9];
+            // karty żeb
+            score += player.pointsList[10];
+            // niewykorszystane pola
+            score += (player.pointsList[11]*(-1));
+            // pokoje * typ
+            score += (player.pointsList[12] * player.pointsList[8]);
+            // punkty z kart
+            score += player.pointsList[13];
+            // bonusy
+            score += player.pointsList[14];
+
+            player.score = score;
+
             StateManager.Set<Player>(selectedPlayer, player);
         }
 
@@ -71,6 +94,11 @@ namespace AgricolaCalculator
                 }
                 y++;
             }
+            unusedSpacesValue.Text = player.pointsList[11].ToString();
+            roomsValue.Text = player.pointsList[12].ToString();
+            cardsPointsValue.Text = player.pointsList[13].ToString();
+            bonusPointsValue.Text = player.pointsList[14].ToString();
+            countTotalScore();
         }
 
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
@@ -221,6 +249,105 @@ namespace AgricolaCalculator
             countTotalScore();
         }
 
+        private void roomTypeBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (ScoreButton roomTypeBtn in roomTypeBtnList)
+            {
+                if (roomTypeBtn.btn.IsPressed)
+                {
+                    player.pointsList[8] = roomTypeBtn.points;
+                }
+                else
+                {
+                    roomTypeBtn.btn.IsChecked = false;
+                    roomTypeBtn.btn.Background = new SolidColorBrush(Colors.Transparent);
+                }
+            }
+            countTotalScore();
+        }
+
+        private void familyMembersBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (ScoreButton familyMemberBtn in familyMembersBtnList)
+            {
+                if (familyMemberBtn.btn.IsPressed)
+                {
+                    player.pointsList[9] = familyMemberBtn.points;
+                }
+                else
+                {
+                    familyMemberBtn.btn.IsChecked = false;
+                    familyMemberBtn.btn.Background = new SolidColorBrush(Colors.Transparent);
+                }
+            }
+            countTotalScore();
+        }
+
+        private void beggingCardsBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (ScoreButton beggingCardBtn in beggingCardsBtnList)
+            {
+                if (beggingCardBtn.btn.IsPressed)
+                {
+                    player.pointsList[10] = beggingCardBtn.points;
+                }
+                else
+                {
+                    beggingCardBtn.btn.IsChecked = false;
+                    beggingCardBtn.btn.Background = new SolidColorBrush(Colors.Transparent);
+                }
+            }
+            countTotalScore();
+        }
+
+        private void unusedSpacesPlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            unusedSpacesValue.Text = (int.Parse(unusedSpacesValue.Text) + 1).ToString();
+            player.pointsList[11] = int.Parse(unusedSpacesValue.Text);
+        }
+
+        private void unusedSpacesMinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            unusedSpacesValue.Text = (int.Parse(unusedSpacesValue.Text) - 1).ToString();
+            player.pointsList[11] = int.Parse(unusedSpacesValue.Text);
+        }
+
+        private void roomsPlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            roomsValue.Text = (int.Parse(roomsValue.Text) + 1).ToString();
+            player.pointsList[12] = int.Parse(roomsValue.Text);
+        }
+
+        private void roomsMinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            roomsValue.Text = (int.Parse(roomsValue.Text) - 1).ToString();
+            player.pointsList[12] = int.Parse(roomsValue.Text);
+        }
+
+        private void cardsPointsPlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            cardsPointsValue.Text = (int.Parse(cardsPointsValue.Text) + 1).ToString();
+            player.pointsList[13] = int.Parse(cardsPointsValue.Text);
+        }
+
+        private void cardsPointsMinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            cardsPointsValue.Text = (int.Parse(cardsPointsValue.Text) - 1).ToString();
+            player.pointsList[13] = int.Parse(cardsPointsValue.Text);
+        }
+
+        private void bonusPointsPlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bonusPointsValue.Text = (int.Parse(bonusPointsValue.Text) + 1).ToString();
+            player.pointsList[14] = int.Parse(bonusPointsValue.Text);
+        }
+
+        private void bonusPointsMinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bonusPointsValue.Text = (int.Parse(bonusPointsValue.Text) - 1).ToString();
+            player.pointsList[14] = int.Parse(bonusPointsValue.Text);
+        }
+
         class ScoreButton
         {
             public int points { get; set; }
@@ -232,5 +359,7 @@ namespace AgricolaCalculator
                 this.btn = btn;
             }
         }
+
+
     }
 }
