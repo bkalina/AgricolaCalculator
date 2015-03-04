92b8e97fddcc002794056553390cc49d0f961dfd
Bartosz Kalina <bartosz.kalina@javamylove.pl>
2015-03-04 11:43:50 +0100

    Aktualizacja polaczenia z DB

--- AgricolaCalculator/MainPage.xaml.cs
+++ AgricolaCalculator/MainPage.xaml.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using SQLiteClient;
 
@@ -17,11 +8,16 @@ namespace AgricolaCalculator
     public partial class MainPage : PhoneApplicationPage
     {
         
-        SQLiteConnection mySQLiteDB = null;
+        public static SQLiteConnection mySQLiteDB = null;
 
         public MainPage()
         {
             InitializeComponent();
+            if (mySQLiteDB == null)
+            {
+                mySQLiteDB = new SQLiteConnection("MyTestDB");
+                mySQLiteDB.Open();
+            }
         }
 
         private void addGame_Click(object sender, RoutedEventArgs e)
@@ -36,7 +32,25 @@ namespace AgricolaCalculator
 
         private void login_Click(object sender, RoutedEventArgs e)
         {
+            SQLiteCommand cmd = mySQLiteDB.CreateCommand("drop table Games");
+            Console.Write(cmd.ExecuteNonQuery());
+            cmd = mySQLiteDB.CreateCommand("create table Games (id text primary key, game_date text)");
+            Console.Write(cmd.ExecuteNonQuery());
+            string id;
+            string gameDate;
+            for (int j = 0; j < 10; j++)
+            {
+                id = Guid.NewGuid().ToString();
+                gameDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                cmd.CommandText = "insert into Games (id, game_date) values (\"" + id + "\",\"" + gameDate + "\")";
+                cmd.ExecuteNonQuery();
+            }
 
+            cmd = mySQLiteDB.CreateCommand("select * from Games");
+            Console.Write(cmd.ExecuteNonQuery());
+            //SqliteDataReader reader = cmd.ExecuteReader();
+            //while (reader.Read())
+            //    Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
         }
     }
 }
